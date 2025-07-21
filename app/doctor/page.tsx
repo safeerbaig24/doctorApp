047e615d09a02909426c4ed26df472a9047b7263
safeerbaig24 <safeer.baig@zweidevs.com>
2025-07-21 20:23:28 +0500

    doctor dashboard

---
+++ app/doctor/page.tsx
@@ -0,0 +1,10 @@
+import { DoctorDashboard } from '@/component/doctor-dashboard'
+import React from 'react'
+
+const DoctorAppPage = () => {
+  return (
+  <DoctorDashboard/>
+  )
+}
+
+export default DoctorAppPage
